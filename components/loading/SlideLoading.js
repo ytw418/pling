0f10cbea064a08b038608614ef6c0fdbc4bac148
ytw418 <ytw418@gmail.com>
2022-06-21 18:35:48 +0900

    refactor 헤더 컴포넌트 리팩토링, 로더 컴포넌트 하나로 모든 로딩 페이지 대응 하도록 리팩토링

--- components/loading/SlideLoading.js
+++ components/loading/SlideLoading.js
@@ -3,22 +3,22 @@ import styled from "styled-components/native";
 import { WINDOW_HEIGHT } from "../../constants";
 
 const SlideLoading = () => {
-  return (
-    <View>
-      <Title>슬라이드 리스트 로딩중</Title>
-    </View>
-  );
+	return (
+		<View>
+			<Title>슬라이드 리스트 로딩중</Title>
+		</View>
+	);
 };
 
 const View = styled.View`
-  background-color: #000;
-  justify-content: center;
-  align-items: center;
-  height: ${WINDOW_HEIGHT / 2}px;
+	background-color: #000;
+	justify-content: center;
+	align-items: center;
+	height: ${WINDOW_HEIGHT / 2}px;
 `;
 const Title = styled.Text`
-  color: #fff;
-  font-size: 18px;
+	color: #fff;
+	font-size: 18px;
 `;
 
 export default SlideLoading;
