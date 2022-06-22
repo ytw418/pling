15a6512d1b245e245074bc556c00ef34a843fee8
ytw418 <ytw418@gmail.com>
2022-06-22 12:04:51 +0900

    refactor 카드 좋아요 클릭시 시놉시스디폴트 카드만 타이틀 색상이 변경되는 문제 수정 원인 = 색상값을 정하기 위해 참조하는 id 를 가진 객체가 2개 이상일 경우 해결방법 = 클릭한 id 를 가진 모든 state 객체들에게 active 값 부여

--- ContextAPI.js
+++ ContextAPI.js
@@ -2,73 +2,73 @@ import React, { useState, useReducer, createContext, useContext } from "react";
 import produce from "immer";
 
 const initials = {
-  // home: {
-  //   slides: false,
-  //   cate: false,
-  // },
+	// home: {
+	//   slides: false,
+	//   cate: false,
+	// },
 };
 
 function Reducer(state, action) {
-  switch (action.type) {
-    case "HOME_LODGING_SLIDES":
-      return {
-        ...state,
-        home: { ...state.home, slides: action.slides },
-      };
-    case "HOME_LODGING_CATE":
-      return {
-        ...state,
-        home: { ...state.home, cate: action.cate },
-      };
-    case "CARD_LIKE":
-      return produce(state, (draft) => {
-        const target = draft.home.cate.find((data) =>
-          data.list.find((obj) => obj.id === action.id)
-        );
-        const targetObj = target.list.find((obj) => obj.id === action.id);
-        targetObj.active = !targetObj.active; // isLiked
-      });
+	switch (action.type) {
+		case "HOME_LODGING_SLIDES":
+			return {
+				...state,
+				home: { ...state.home, slides: action.slides },
+			};
+		case "HOME_LODGING_CATE":
+			return {
+				...state,
+				home: { ...state.home, cate: action.cate },
+			};
+		case "CARD_LIKE":
+			return produce(state, (draft) => {
+				draft.home.cate.map((item) => {
+					const targetObj = item.list.find((obj) => obj.id === action.id);
+					console.log("draft.home.cate", targetObj);
+					if (targetObj) targetObj.active = !targetObj.active; // isLiked
+				});
+			});
 
-    // return {
-    //   ...state,home.cate:{}
-    //  home: { ...state.home, cate: {...state.home.cate} },
+		// return {
+		//   ...state,home.cate:{}
+		//  home: { ...state.home, cate: {...state.home.cate} },
 
-    // };
-    case "textSend":
-      return { ...state, text: action.text };
+		// };
+		case "textSend":
+			return { ...state, text: action.text };
 
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+		default:
+			throw new Error(`Unhandled action type: ${action.type}`);
+	}
 }
 const StateContext = createContext();
 const DispatchContext = createContext();
 
 export function Provider({ children }) {
-  const [state, dispatch] = useReducer(Reducer, initials);
+	const [state, dispatch] = useReducer(Reducer, initials);
 
-  return (
-    <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={dispatch}>
-        {children}
-      </DispatchContext.Provider>
-    </StateContext.Provider>
-  );
+	return (
+		<StateContext.Provider value={state}>
+			<DispatchContext.Provider value={dispatch}>
+				{children}
+			</DispatchContext.Provider>
+		</StateContext.Provider>
+	);
 }
 
 // 커스텀 hook 과 에러처리 구문
 export function useApiState() {
-  const context = useContext(StateContext);
-  if (!context) {
-    throw new Error("Cannot find Provider");
-  }
-  return context;
+	const context = useContext(StateContext);
+	if (!context) {
+		throw new Error("Cannot find Provider");
+	}
+	return context;
 }
 
 export function useDispatch() {
-  const context = useContext(DispatchContext);
-  if (!context) {
-    throw new Error("Cannot find Provider");
-  }
-  return context;
+	const context = useContext(DispatchContext);
+	if (!context) {
+		throw new Error("Cannot find Provider");
+	}
+	return context;
 }
